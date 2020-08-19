fbdf5a5c134a07ded827b07050c57731c3653b46
28111999lenhathoang <53230244+28111999lenhathoang@users.noreply.github.com>
2020-08-19 08:50:35 +0700

    Add project files.

---
+++ ObservableCollectionDemo/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionDemo.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatarpath { get; set; }
+
+    }
+}
